33a579bd10e2f179234cc27f506f31f30e8d1a61
Filleul Antoine <antoine.filleul@epitech.eu>
2023-05-05 14:54:29 +0200

    [add] line editing without history

---
+++ src/str/get_input.c
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** get_input
+*/
+
+int get_dir(int *index, int c)
+{
+    read(0, &c, 1);
+    read(0, &c, 1);
+    switch (c) {
+    case 'C':
+        if (index[0] < index[1] - 2) {
+            index[0] += 1;
+        }
+        break;
+    case 'D':
+        if (index[0] > -1) {
+            index[0] += -1;
+        }
+        break;
+    }
+    return 0;
+}
+
+char *get_ch(int c, char *command, int *index)
+{
+    if (c == 27) {
+        get_dir(index, c);
+        return command;
+    }
+    if (c == 127) {
+        if (index[0] != -1)
+            command = sup_char(command, index);
+    } else {
+        command = add_char(command, c, index);
+    }
+    return command;
+}
